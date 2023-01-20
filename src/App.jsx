c182558ef4dd0e7ef5c5f01ce839821f759d41a1
laura <godoyvalentina612@gmail.com>
2023-01-20 15:20:49 -0500

    documentos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,51 @@
 import './App.css';
 import Pag from './Components/contentPag.jsx';
+import Menu from './Components/Menu';
+import Opciones from './Components/opciones';
 
 function App() {
  
 
   return (
-   <Pag />
-  )
+  <body>
+  <article id="#home"/>
+  <header class="menu">
+    <img class="logo" src="./images/logop.jpg"/>
+  <nav class="menuu">
+    <ul>
+   <Menu nombre='inicio'
+   link='index.html'/>,
+   <Menu nombre='contacto'
+   link='contacto.html'/>,
+   <Menu nombre='sobre nosotros'
+   link='nosotros.html'/>,
+   <Menu nombre='galeria'
+   link='galeria.html'/>,
+    </ul>
+  </nav>
+</header>
+<form action="#">
+  <select name="Proceso" class="botones" id="lang">
+  <Opciones nombre='Seleccionar'
+  link='Seleccionar'/>,
+  <Opciones nombre='Extensiones de pestañas'
+  link='pestañas'/>,
+  <Opciones nombre='Cejas'
+  link='cejas'/>,
+  <Opciones nombre='Lifting'
+  link='lifting'/>,
+
+  </select>
+  </form>
+  </body>
+
+  );
 }
+
+function App( {
+  return(
+    <h1>App</h1>
+  )
+})
 
 export default App
---
+++ src/data.js
@@ -0,0 +1,9 @@
+export const data = [
+    {
+        id: 1,
+        nameProduct: 'Pestañas efecto natural',
+        price: 70,
+        urlImage: '',
+        quentify: 1,
+    },
+]
